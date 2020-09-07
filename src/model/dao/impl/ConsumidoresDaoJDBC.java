bd105e6b00d2b30eface615d2b4a526f2f48281d
Your Name <you@example.com>
2020-09-07 09:49:19 -0300

    ConsumidoresDaoJDBC e DaoFactory

---
+++ src/model/dao/impl/ConsumidoresDaoJDBC.java
@@ -0,0 +1,40 @@
+package model.dao.impl;
+
+import java.util.List;
+
+import model.dao.ConsumidoresDao;
+import model.entities.Consumidores;
+
+public class ConsumidoresDaoJDBC implements ConsumidoresDao {
+
+	@Override
+	public void insert(Consumidores obj) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void update(Consumidores obj) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void deleteById(Integer id) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Consumidores findById(Integer id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Consumidores> findAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
